aec6d6e5a394f382e496a783820e5afbf98db357
Arnaud Ambroselli <arnaud.ambroselli@yahoo.fr>
2017-11-20 12:02:40 +0100

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import { uniq } from 'lodash';
+import jsonp from 'jsonp';
+import insane from 'insane';
+import 'bootstrap';
+
+const ages = [1,1,4,52,12,4];
+
+console.log(uniq(ages));
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const nodeEnv = process.env.NODE_ENV || 'production';
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "./src/css/styles.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    app: [
+    './src/components/app.js',
+    './src/scss/styles.scss'
+    ]
+  },
+  output: {
+    filename: './_build/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+            use: [{
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader"
+            }, {
+            loader: 'postcss-loader'
+          }],
+            // use style-loader in development
+            fallback: "style-loader"
+        })
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        },
+      }
+    ]
+  },
+  plugins: [
+    //uglify js
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      sourceMap: true
+    }),
+    // env plugin
+    // new webpack.DefinePlugin({
+    //   'process.env': { NODE_ENV: JSON.stringify(nodeEnv)}
+    // }),
+    //browser-sync
+    extractSass,
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+      // In case you imported plugins individually, you must also require them here:
+      // Util: "exports-loader?Util!bootstrap/js/dist/util",
+      // Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+    }),
+    // new StaticSiteGeneratorPlugin('main', data.routes, data),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      files: ['./*.html', './public/*.html', './src/css/styles.css'],
+      server: { baseDir: ['./']}
+    })
+    ],
+    watch: true
+  }
